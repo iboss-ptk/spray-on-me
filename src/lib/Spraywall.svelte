4d6dcf5f040e9cb87d292db97e2bf42021e6df08
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2024-07-11 16:53:56 +0700

    add all info

--- src/lib/Spraywall.svelte
+++ src/lib/Spraywall.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import holds_json from '$lib/holds.json';
-	import { HOLD_USAGE, RESTRICTION, type Hold } from './types';
+	import { HOLD_USAGE, RESTRICTION, type Hold, type Grade } from './types';
 	import HoldComponent from './Hold.svelte';
 	import * as ToggleGroup from '$lib/components/ui/toggle-group';
 	import * as Card from '$lib/components/ui/card';
+	import * as Separator from '$lib/components/ui/separator';
 
 	import { onMount } from 'svelte';
+	import Textarea from './components/ui/textarea/textarea.svelte';
 
 	let holds: Hold[] = holds_json.map((hold) => ({
 		...hold,
 		usage: HOLD_USAGE.not_included,
 		restriction: RESTRICTION.none
 	}));
+
+	let grade: Grade = 'v0';
+
 	let holdUsage = HOLD_USAGE.any_move;
 	let restriction = RESTRICTION.none;
 
@@ -63,41 +68,77 @@
 	</div>
 </div>
 
-<Card.Root class="m-4">
-	<Card.Header>
-		<Card.Title>Usage</Card.Title>
-		<Card.Description
-			>Determine if the selecting hold is used for any move, start, or finish.</Card.Description
-		>
-	</Card.Header>
-	<Card.Content>
-		<ToggleGroup.Root type="single" bind:value={holdUsage}>
-			<ToggleGroup.Item value={HOLD_USAGE.any_move}>
-				<div class="bg-indigo-300 w-2 h-2 rounded-full p-2 mr-2" />
-				any move
-			</ToggleGroup.Item>
-			<ToggleGroup.Item value={HOLD_USAGE.start}>
-				<div class="bg-lime-300 w-2 h-2 rounded-full p-2 mr-2" />
-				start
-			</ToggleGroup.Item>
-			<ToggleGroup.Item value={HOLD_USAGE.finish}>
-				<div class="bg-rose-300 w-2 h-2 rounded-full p-2 mr-2" />
-				finish
-			</ToggleGroup.Item>
-		</ToggleGroup.Root>
-	</Card.Content>
-</Card.Root>
+<Separator.Root class="my-4" />
 
-<Card.Root class="m-4">
-	<Card.Header>
-		<Card.Title>Restriction</Card.Title>
-		<Card.Description>Determine the restriction for the selected hold.</Card.Description>
-	</Card.Header>
-	<Card.Content>
-		<ToggleGroup.Root type="single" bind:value={restriction}>
-			<ToggleGroup.Item value={RESTRICTION.none}>none</ToggleGroup.Item>
-			<ToggleGroup.Item value={RESTRICTION.hands}>🙌 hands</ToggleGroup.Item>
-			<ToggleGroup.Item value={RESTRICTION.feet}>👟 feet</ToggleGroup.Item>
-		</ToggleGroup.Root>
-	</Card.Content>
-</Card.Root>
+<div class="my-4">
+	<Card.Root class="m-4">
+		<Card.Header>
+			<Card.Title>Usage</Card.Title>
+			<Card.Description
+				>Determine if the selecting hold is used for any move, start, or finish.</Card.Description
+			>
+		</Card.Header>
+		<Card.Content>
+			<ToggleGroup.Root type="single" bind:value={holdUsage}>
+				<ToggleGroup.Item value={HOLD_USAGE.any_move}>
+					<div class="bg-indigo-300 w-2 h-2 rounded-full p-2 mr-2" />
+					any move
+				</ToggleGroup.Item>
+				<ToggleGroup.Item value={HOLD_USAGE.start}>
+					<div class="bg-lime-300 w-2 h-2 rounded-full p-2 mr-2" />
+					start
+				</ToggleGroup.Item>
+				<ToggleGroup.Item value={HOLD_USAGE.finish}>
+					<div class="bg-rose-300 w-2 h-2 rounded-full p-2 mr-2" />
+					finish
+				</ToggleGroup.Item>
+			</ToggleGroup.Root>
+		</Card.Content>
+	</Card.Root>
+
+	<Card.Root class="m-4">
+		<Card.Header>
+			<Card.Title>Restriction</Card.Title>
+			<Card.Description>Determine the restriction for the selected hold.</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<ToggleGroup.Root type="single" bind:value={restriction}>
+				<ToggleGroup.Item value={RESTRICTION.none}>none</ToggleGroup.Item>
+				<ToggleGroup.Item value={RESTRICTION.hands}>🙌 hands</ToggleGroup.Item>
+				<ToggleGroup.Item value={RESTRICTION.feet}>👟 feet</ToggleGroup.Item>
+			</ToggleGroup.Root>
+		</Card.Content>
+	</Card.Root>
+</div>
+
+<div class="my-4">
+	<h2 class="text-xl font-bold m-4">Route Info</h2>
+
+	<Card.Root class="m-6">
+		<Card.Header>
+			<Card.Title>Grade</Card.Title>
+			<Card.Description>Climbing grade for this route.</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<ToggleGroup.Root class="flex-wrap" type="single" bind:value={grade}>
+				{#each ['v0', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9', 'v10', 'v11', 'v12', 'v13', 'v14', 'v15', 'v16', 'v17', 'v18', 'v19', 'v20'] as gradeValue}
+					<ToggleGroup.Item value={gradeValue}>
+						{gradeValue}
+					</ToggleGroup.Item>
+				{/each}
+			</ToggleGroup.Root>
+		</Card.Content>
+	</Card.Root>
+
+	<Card.Root class="m-6">
+		<Card.Header>
+			<Card.Title>Description</Card.Title>
+			<Card.Description
+				>Describe the route, how to use, additional restrictions and any other relevant info.</Card.Description
+			>
+		</Card.Header>
+		<Card.Content>
+			<Textarea placeholder={`Eg. Crossover only, campus only`}></Textarea>
+		</Card.Content>
+	</Card.Root>
+</div>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,6 +7,29 @@ export const HOLD_USAGE: { [key in HoldUsage]: HoldUsage } = {
 	finish: 'finish'
 };
 
+export type Grade =
+	| 'v0'
+	| 'v1'
+	| 'v2'
+	| 'v3'
+	| 'v4'
+	| 'v5'
+	| 'v6'
+	| 'v7'
+	| 'v8'
+	| 'v9'
+	| 'v10'
+	| 'v11'
+	| 'v12'
+	| 'v13'
+	| 'v14'
+	| 'v15'
+	| 'v16'
+	| 'v17'
+	| 'v18'
+	| 'v19'
+	| 'v20';
+
 export type Restriction = 'none' | 'hands' | 'feet';
 
 export const RESTRICTION: { [key in Restriction]: Restriction } = {
